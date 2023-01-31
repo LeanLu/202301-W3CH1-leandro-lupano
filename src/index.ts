3bc4ba87ee4dccbf50d1930bed89e56b64816974
Leandro <leandrolupano@hotmail.com>
2023-01-31 20:58:13 +0100

    Link index.html y ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import { Component } from './components/component/component';
+import { Container } from './components/container/container';
+import { Card } from './components/card/card';
+import { characters } from './mocks/characters';
+
 import './style.scss';
+
+new Container('.container');
+
+new Card('.characters-list');
